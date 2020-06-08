4b44be7de3057bde822a29be3a7d5e8d8ada8d65
Peppe3009 <ventrone524@gmail.com>
2020-06-08 19:35:24 +0200

    bla

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
 window.addEventListener('load', function() {
     navigator.serviceWorker.register('../sw.js').then(function(registration) {
-      console.log('Service Worker registration was successful with scope: ', registration.scope);
+      console.log('Service Worker ok', registration.scope);
     }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
+      console.log('ServiceWorker no ok ', err);
     });
   });
 }
